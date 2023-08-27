7f97df28b9a9f086f3eac1908685eba33c1f3c1f
Cloud User <ec2-user@ip-172-31-89-182.ec2.internal>
2023-08-27 15:47:09 +0000

    first commit

---
+++ exercicio8.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// Função para criar a matriz transposta
+void criarTransposta(int matriz[][3], int transposta[][3], int linhas, int colunas) {
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            transposta[j][i] = matriz[i][j];
+        }
+    }
+}
+
+int main() {
+    int matriz[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    
+    int transposta[3][3];
+
+    criarTransposta(matriz, transposta, 3, 3);
+
+    printf("Matriz original:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nMatriz transposta:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", transposta[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+# SO_exercicios
